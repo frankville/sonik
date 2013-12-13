472b033b3f6dd8c4c0451c3a70afdcdd0ee18bbe
frank <frank@distrodev.com>
2013-12-13 00:07:10 -0300

    primer commit test android(corregido)

--- Test/src/distro/sonik/test/MainActivity.java
+++ Test/src/distro/sonik/test/MainActivity.java
@@ -1,11 +1,16 @@
-package com.distro.test;
+package distro.sonik.test;
 
-import android.os.Bundle;
+import com.distro.test.R;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	public final String EXTRA_MESSAGE = "distro.sonik.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +23,13 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    public void sendMessage(View v){
+    	Intent intent = new Intent(this, DisplayMessageActivity.class);
+    	EditText editField = (EditText)this.findViewById(R.id.editField);
+    	String message = editField.getText().toString();
+    	intent.putExtra(EXTRA_MESSAGE, message);
     }
     
 }
